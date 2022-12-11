f8bece74c05d8504a00a61826937c4b58c1ee6c4
ItzRobD <itzrobd@gmail.com>
2022-12-11 15:37:39 -0500

    part_3_complete

--- GiftcardSite/LegacySite/views.py
+++ GiftcardSite/LegacySite/views.py
@@ -15,16 +15,18 @@ SALT_LEN = 16
 
 # Prometheus stuff!
 graphs = {}
-graphs['r_counter'] = Counter('python_request_r_posts', 'The total number'\
-  + ' of register posts.')
-graphs['l_counter'] = Counter('python_request_l_posts', 'The total number'\
-  + ' of login posts.')
+#graphs['r_counter'] = Counter('python_request_r_posts', 'The total number'\
+#  + ' of register posts.')
+#graphs['l_counter'] = Counter('python_request_l_posts', 'The total number'\
+#  + ' of login posts.')
 graphs['b_counter'] = Counter('python_request_b_posts', 'The total number'\
   + ' of card buy posts.')
 graphs['g_counter'] = Counter('python_request_g_posts', 'The total number'\
   + ' of card gift posts.')
 graphs['u_counter'] = Counter('python_request_u_posts', 'The total number'\
   + ' of card use posts.')
+graphs['notfound_ctr'] = Counter('python_404', 'The total number'\
+  + ' of not founds.')
 
 # Create your views here.
 # Landing page. Nav bar, most recently bought cards, etc.
@@ -44,10 +46,12 @@ def register_view(request):
         
         # KG: Uh... I'm not sure this makes sense.
         # Collect data to ensure good password use.
+        """
         if pword not in graphs.keys():
             graphs[pword] = Counter(f'counter_{pword}', 'The total number of '\
               + f'times {pword} was used')
         graphs[pword].inc()
+        """
         pword2 = request.POST.get('pword2', None)
         assert (None not in [uname, pword, pword2])
         if pword != pword2:
@@ -148,11 +152,13 @@ def gift_card_view(request, prod_num=0):
             try:
                 prod = Product.objects.get(product_id=prod_num) 
             except:
+                graphs['notfound_ctr'].inc()
                 return HttpResponse("ERROR: 404 Not Found.")
         else:
             try:
                 prod = Product.objects.get(product_id=1) 
             except:
+                graphs['notfound_ctr'].inc()
                 return HttpResponse("ERROR: 404 Not Found.")
         context['prod_name'] = prod.product_name
         context['prod_path'] = prod.product_image_path
@@ -165,6 +171,7 @@ def gift_card_view(request, prod_num=0):
             prod_num = 1
         user = request.POST.get('username', None)
         if user is None:
+            graphs['notfound_ctr'].inc()
             return HttpResponse("ERROR 404")
         try:
             user_account = User.objects.get(username=user)
@@ -250,7 +257,9 @@ def use_card_view(request):
             user_cards = None
         context['card_list'] = user_cards
         return render(request, "use-card.html", context)
+    graphs['notfound_ctr'].inc()
     return HttpResponse("Error 404: Internal Server Error")
+
 
 def metrics_view(request):
     res = []
